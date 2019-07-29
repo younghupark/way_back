e73fed34729b354fd4aef4ca8c14eb48d54c304b
Piano <piano@Kats-MBP.localdomain>
2019-07-29 17:02:08 -0400

    added loop to extract web data from multiple dates

--- run.py
+++ run.py
@@ -1,18 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "http://archive.org/wayback/available?url=nytimes.com/subscription&timestamp=20060101I"
-
-r = requests.get(url = URL)
 
 
-data=r.json()
+from datetime import timedelta, date
+
+start_date = date(2016, 1, 1)
+end_date = date(2017, 1, 1)
+delta = timedelta(days=1)
+while start_date <= end_date:
+    URL = "http://archive.org/wayback/available?url=nytimes.com/subscription&timestamp=%sI" % start_date.strftime("%Y%m%d")
+
+    r = requests.get(url = URL)
+    data=r.json()
 
 
-main_url = data['archived_snapshots']['closest']['url']
+    main_url = data['archived_snapshots']['closest']['url']
 
-result = requests.get(main_url)
+    result = requests.get(main_url)
 
-soup = BeautifulSoup(result.text, 'html.parser')
+    soup = BeautifulSoup(result.text, 'html.parser')
 
-print(soup.prettify()[:1000])
+    print(soup.prettify()[:1000])
+    start_date += delta
